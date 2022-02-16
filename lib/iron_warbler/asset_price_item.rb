a8a8da81d8706846e8a4331bd76e9fe98a11f37c
piousbox <admin@piousbox.com>
2022-02-16 12:27:08 -0500

    hard-req active_record

--- lib/iron_warbler/asset_price_item.rb
+++ lib/iron_warbler/asset_price_item.rb
@@ -1,4 +1,6 @@
 
+require 'active_record'
+
 class IronWarbler::AssetPriceItem < ActiveRecord::Base
   ## datetime, open, high, low, close, adjusted close, volume
 end
--- lib/iron_warbler/option_price_item.rb
+++ lib/iron_warbler/option_price_item.rb
@@ -1,4 +1,6 @@
 
+require 'active_record'
+
 class IronWarbler::OptionPriceItem < ActiveRecord::Base
 
   self.table_name = 'option_price_items'
