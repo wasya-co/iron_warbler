53006136b7c5d04360f01ab39540cbaee666518f
mac_a2141 <victor@piousbox.com>
2023-12-16 18:37:49 -0500

    autobuild `date "+%Y%m%d"`

--- iron_warbler.gemspec
+++ iron_warbler.gemspec
@@ -1,7 +1,7 @@
 
 Gem::Specification.new do |spec|
   spec.name        = 'iron_warbler'
-  spec.version     = '2.0.6'
+  spec.version     = '2.0.7'
   spec.authors     = [ 'Victor Pudeyev' ]
   spec.email       = 'victor@wasya.co'
   spec.homepage    = 'https://wasya.co'
--- test/dummy/spec/routing/iro/alerts_routing_spec.rb
+++ test/dummy/spec/routing/iro/alerts_routing_spec.rb
@@ -1,11 +1,10 @@
 
-module Iro
-  RSpec.describe AlertsController, type: :routing do
-    describe "routing" do
-      it "routes to #index" do
-        expect(get: "/alerts").to route_to("alerts#index")
-      end
-
-    end
-  end
-end
+# module Iro
+#   RSpec.describe AlertsController, type: :routing do
+#     describe "routing" do
+#       it "routes to #index" do
+#         expect(get: "/alerts").to route_to("alerts#index")
+#       end
+#     end
+#   end
+# end
