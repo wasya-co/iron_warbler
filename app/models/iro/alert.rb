16cb52acf52d3017b318d14d044bbe6759a44785
mac_a2141 <victor@piousbox.com>
2023-12-20 20:25:52 -0500

    autobuild `date "+%Y%m%d"`

--- app/models/iro/alert.rb
+++ app/models/iro/alert.rb
@@ -2,10 +2,21 @@
 class Iro::Alert < Iro::ApplicationRecord
   self.table_name = 'iro_alerts'
 
+
   DIRECTION_ABOVE = 'ABOVE'
   DIRECTION_BELOW = 'BELOW'
   def self.directions_list
     [ nil, DIRECTION_ABOVE, DIRECTION_BELOW ]
   end
+
+  STATUS_ACTIVE   = 'active'
+  STATUS_INACTIVE = 'inactive'
+  STATUSES        = [ 'active', 'inactive' ]
+
+
+  def self.active
+    where( status: STATUS_ACTIVE )
+  end
+
 
 end
