b858669068e5af5795615bc3d2241ec92574f20f
mac_a2141 <victor@piousbox.com>
2024-08-09 22:04:57 -0500

    can show what is the actual max pain is.

--- app/controllers/iro/stocks_controller.rb
+++ app/controllers/iro/stocks_controller.rb
@@ -66,6 +66,25 @@ class Iro::StocksController < Iro::ApplicationController
       symbol: @stock.ticker,
     }).order_by({ date: :desc }).limit(100)
 
+    filename = "./data/schwab/#{Time.now.to_date.to_s}-#{@stock.ticker}-chains.json"
+    if File.exists? filename
+      hash = JSON.parse File.read filename
+    else
+      hash = Tda::Option.get_chains({ ticker: @stock.ticker })
+      File.write filename, hash.to_json
+    end
+    @max_pain = Iro::Option.max_pain hash
+    @max_pain_summary = {}
+    @max_pain.each do |date, types|
+      all = types['all']
+      @max_pain_summary[date] = all.keys[0]
+      all.each do |strike, amount|
+        if amount < all[@max_pain_summary[date]]
+          @max_pain_summary[date] = strike
+        end
+      end
+    end
+
     respond_to do |format|
       format.html
       format.json do
